a4732ed9366745abbead25f5fc7009f3154fef57
Kevin Bowersox <kmb385@gmail.com>
2012-12-01 04:31:10 -0500

    Managed imports [Java]

--- src/test/java/org/tothought/repositories/ExperienceDetailRepositoryTest.java
+++ src/test/java/org/tothought/repositories/ExperienceDetailRepositoryTest.java
@@ -1,8 +1,8 @@
 package org.tothought.repositories;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
 
